1748beba05116b567d491c975f23383ec3683445
Nhance <nhance@Nhances-MacBook-Air.local>
2018-09-01 14:43:54 +0530

    added proxy configuration, style imporvments

--- src/app/rental/rental.module.ts
+++ src/app/rental/rental.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
+import { NgPipesModule } from 'ngx-pipes';
 
 import { RentalComponent } from './rental.component';
 import { RentalListComponent } from './rental-list/rental-list.component';
 import { RentalListItemComponent } from './rental-list-item/rental-list-item.component';
 import { RentalService } from './rental.service';
 import { RentalDetailComponent } from './rental-detail/rental-detail.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import { UppercasePipe } from '../common/pipes/uppercase';
 
 
 const routes: Routes = [
@@ -23,9 +27,12 @@ const routes: Routes = [
         RentalComponent,
         RentalListComponent,
         RentalListItemComponent,
-        RentalDetailComponent
+        RentalDetailComponent,
+        UppercasePipe
     ],
     imports: [CommonModule,
+    HttpClientModule,
+    NgPipesModule,
     RouterModule.forChild(routes)],
     providers:[RentalService]
 })
